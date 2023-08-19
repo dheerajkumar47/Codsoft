ddeee589f9091730fc86fae9faa9cdcd3f8513dd
dheeraj kumar <mr.rathi047@gmail.com>
2023-08-19 11:30:35 +0500

    Updated code

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,4 @@
-const darkTheme = "dark.css";
+const darkTheme = "styles.css";
 const res = document.getElementById("result");
 const toast = document.getElementById("toast");
 
@@ -14,7 +14,6 @@ function calculate(value) {
   }
 }
 
-// Displays entered value on screen.
 function liveScreen(enteredValue) {
   if (!res.value) {
     res.value = "";
@@ -22,66 +21,40 @@ function liveScreen(enteredValue) {
   res.value += enteredValue;
 }
 
-//adding event handler on the document to handle keyboard inputs
 document.addEventListener("keydown", keyboardInputHandler);
 
-//function to handle keyboard inputs
 function keyboardInputHandler(e) {
-  // to fix the default behavior of browser,
-  // enter and backspace were causing undesired behavior when some key was already in focus.
   e.preventDefault();
-  //grabbing the liveScreen
 
-  //numbers
-  if (e.key === "0") {
-    res.value += "0";
-  } else if (e.key === "1") {
-    res.value += "1";
-  } else if (e.key === "2") {
-    res.value += "2";
-  } else if (e.key === "3") {
-    res.value += "3";
-  } else if (e.key === "4") {
-    res.value += "4";
-  } else if (e.key === "5") {
-    res.value += "5";
-  } else if (e.key === "6") {
-    res.value += "6";
-  } else if (e.key === "7") {
-    res.value += "7";
-  } else if (e.key === "7") {
-    res.value += "7";
-  } else if (e.key === "8") {
-    res.value += "8";
-  } else if (e.key === "9") {
-    res.value += "9";
-  }
+  const keyMappings = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    "+": "+",
+    "-": "-",
+    "*": "*",
+    "/": "/",
+    ".": ".",
+    Enter: "calculate",
+    Backspace: "backspace",
+  };
 
-  //operators
-  if (e.key === "+") {
-    res.value += "+";
-  } else if (e.key === "-") {
-    res.value += "-";
-  } else if (e.key === "*") {
-    res.value += "*";
-  } else if (e.key === "/") {
-    res.value += "/";
-  }
+  const keyPressed = keyMappings[e.key];
 
-  //decimal key
-  if (e.key === ".") {
-    res.value += ".";
-  }
-
-  //press enter to see result
-  if (e.key === "Enter") {
-    calculate(result.value);
-  }
-
-  //backspace for removing the last input
-  if (e.key === "Backspace") {
-    const resultInput = res.value;
-    //remove the last element in the string
-    res.value = resultInput.substring(0, res.value.length - 1);
+  if (keyPressed) {
+    if (keyPressed === "calculate") {
+      calculate(result.value);
+    } else if (keyPressed === "backspace") {
+      res.value = res.value.slice(0, -1);
+    } else {
+      res.value += keyPressed;
+    }
   }
 }
